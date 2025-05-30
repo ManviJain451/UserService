1e080188a2a726998dadad3c6114a410b915cee8
manvi <manvi.jain1@tothenew.com>
2025-05-30 17:10:16 +0530

    Added configurations for SSL encryption

--- src/main/java/com/microservice/userservice/UserserviceApplication.java
+++ src/main/java/com/microservice/userservice/UserserviceApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserserviceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(UserserviceApplication.class, args);
+		System.setProperty("javax.net.debug", "ssl:handshake");
 	}
 
 }
--- src/main/java/com/microservice/userservice/config/AppConfig.java
+++ src/main/java/com/microservice/userservice/config/AppConfig.java
@@ -1,11 +1,21 @@
 package com.microservice.userservice.config;
 
-import jakarta.annotation.PostConstruct;
-import lombok.Getter;
+
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.io.HttpClientConnectionManager;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.core5.ssl.SSLContextBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.SSLContext;
+import java.security.KeyStore;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -20,6 +30,18 @@ public class AppConfig {
     @Value("${rsa.note-public-key-path}")
     private String notePublicKeyPath;
 
+    @Value("${client.ssl.key-store}")
+    private Resource keyStore;
+
+    @Value("${client.ssl.key-store-password}")
+    private String keyStorePassword;
+
+    @Value("${client.ssl.trust-store}")
+    private Resource trustStore;
+
+    @Value("${client.ssl.trust-store-password}")
+    private String trustStorePassword;
+
     @Bean
     public PublicKey noteServicePublicKey() throws Exception {
         byte[] keyBytes = Files.readAllBytes(Paths.get(notePublicKeyPath.replace("classpath:", "src/main/resources/")));
@@ -33,9 +55,33 @@ public class AppConfig {
         return kf.generatePublic(spec);
     }
 
+
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate() throws Exception {
+        KeyStore keyStoreInstance = KeyStore.getInstance("JKS");
+        keyStoreInstance.load(keyStore.getInputStream(), keyStorePassword.toCharArray());
+
+        KeyStore trustStoreInstance = KeyStore.getInstance("JKS");
+        trustStoreInstance.load(trustStore.getInputStream(), trustStorePassword.toCharArray());
+
+        SSLContext sslContext = SSLContextBuilder.create()
+                .loadKeyMaterial(keyStoreInstance, keyStorePassword.toCharArray())
+                .loadTrustMaterial(trustStoreInstance, null)
+                .build();
+
+        SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(sslContext);
+
+        HttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder.create()
+                .setSSLSocketFactory(socketFactory)
+                .build();
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setConnectionManager(connectionManager)
+                .build();
+
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setHttpClient(httpClient);
+        return new RestTemplate(factory);
     }
 
 }
